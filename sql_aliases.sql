c7fc7600a0ed167e7403d73037b0a211a339cf72
Harshad Kulkarni <harshad@Harshads-MacBook-Air.local>
2023-08-10 17:48:37 -0500

    Added new sql query that uses SQL ALIASES

---
+++ sql_aliases.sql
@@ -0,0 +1,5 @@
+--SQL aliases are used to give a table, or a column in a table, a temporary name
+--An alias is created with the AS keyword
+
+SELECT CustomerID AS ID, CustomerName AS Customer
+FROM Customers;
